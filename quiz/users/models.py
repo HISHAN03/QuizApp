5b96c83d1c9c9991c89eb1d960471f01a37f128d
HISHAN03 <moidinhishan@gmail.com>
2024-03-10 13:54:57 +0530

    formatting

--- quiz/users/models.py
+++ quiz/users/models.py
@@ -5,9 +5,6 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     usn = models.CharField(max_length=20)
-
-    # Add any additional fields or methods as needed
-
     def __str__(self):
         return self.email
 
