7a58e8d13367f221a8cf6f43a217800917e3e1e0
HISHAN03 <moidinhishan@gmail.com>
2024-03-12 08:34:09 +0530

    readme-update

--- quiz/users/utils.py
+++ quiz/users/utils.py
@@ -15,38 +15,29 @@ def add_New_value_to_database( quizid,quiz_name, question,op1,op2,op3,op4,ans):
         cursor.execute(sql_query1)
         cursor.execute(sql_query2)
 
-
 def register_user(usn,email,password):
     with connection.cursor() as cursor:
         sql_query = f"INSERT INTO student( usn,emailid, password) VALUES ( {usn}, '{email}','{password}');"
         cursor.execute(sql_query)
-
-
 
 def My_login1(usn, email1, password1):
     with connection.cursor() as cursor:
         sql_query = "SELECT emailid, password FROM student;"
         cursor.execute(sql_query)
         results = cursor.fetchall()
-
-
         for result in results:
             emailid, password = result
             if email1 == emailid and password1 == password:
                 return True
-
         return False
-
-
+        
 def staff_signup1(email1, password1):
     with connection.cursor() as cursor:
         sql_query = "SELECT email, password FROM staff_login;"
         cursor.execute(sql_query)
         results = cursor.fetchall()
-
         for result in results:
             emailid, password = result
             if email1 == emailid and password1 == password:
                 return True
-
         return False
